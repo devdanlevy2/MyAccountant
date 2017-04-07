5734949f6930df8ed2a6a857439fa9b45daf60b1
devdanlevy <daniel.levy2@icloud.com>
2017-04-06 23:07:49 -0400

    reconfiguring routes and adding header

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
 
 
   resources :accounts
+  get 'accounts' => 'accounts#index'
   get 'accounts/new'
   put 'accounts/update'
+  get 'users/new'
+  put 'users/update'
+  delete 'users/delete'
   delete 'accounts/delete'
   root 'welcome#index'
   get 'welcome/index' => 'welcome#index'
-    devise_for :users
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
